5a56c82c113536de677c3ee4c1bbe11ed8896c1b
jordanperkins98 <jordanperkins@hotmail.co.uk>
2021-06-20 20:31:29 +0100

    first commit

---
+++ scipt.py
@@ -0,0 +1,15 @@
+import os, sys
+from pytube import YouTube
+
+#takes the first argument after the file when calling
+link = sys.argv[1]
+print(link)
+try:
+    #change working directory
+    os.chdir("/home/jperkins/Downloads")
+except OSError:
+    print(os.getcwd())
+    print("Cant change directory")
+
+#Uses Pytube download() to download file
+print(YouTube(link).streams.get_highest_resolution().download())
